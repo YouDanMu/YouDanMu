ced32389092af8b437b7f0777d3869aead1aeed3
RixTox <i@rix.li>
2017-02-12 18:31:04 -0500

    Danmaku layout, animation implementation

--- src/ts/content-script/YDM.ts
+++ src/ts/content-script/YDM.ts
@@ -1,4 +1,5 @@
 import { Danmaku } from './Danmaku';
+import { Segment, Segments } from './Segments';
 
 export class YDM {
     static PlayerState = {
@@ -10,39 +11,39 @@ export class YDM {
         CUED: 5
     };
 
-    dom: {[key:string]: HTMLElement} = {};
+    moviePlayer: HTMLElement;
+    overlay: SVGElement;
+    prepare: SVGElement;
+    player: any;
+    state = YDM.PlayerState.UNSTARTED;
+    danmaku: {[key:string]: Danmaku[]} = {
+        [Danmaku.MODE.TOP]: [],
+        [Danmaku.MODE.BOTTOM]: [],
+        [Danmaku.MODE.MARQUEE]: []
+    };
+    segments: {[key:string]: Segments} = {
+        [Danmaku.MODE.TOP]: null,
+        [Danmaku.MODE.BOTTOM]: null,
+        [Danmaku.MODE.MARQUEE]: null
+    };
+    settings: {[key:string]: any} = {
+        'duration': 5000, // in millisecond
+        'speed': 1,
+    };
+
+    resumeTimeVideo: number;
+    resumeTimeFrame: number;
 
     constructor() {
+        this.prepare = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+        this.prepare.id = 'ydm-prepare';
+        this.overlay = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+        this.overlay.id = 'ydm-overlay';
         document.addEventListener('DOMContentLoaded', (...args: any[]) => {
             this.log('DOMContentLoaded');
+            document.body.appendChild(this.prepare);
             this.hijectLoadFunction();
-            let oldReady = (<any>window).onYouTubePlayerReady;
-            (<any>window).onYouTubePlayerReady = (player: any) => {
-                this.log('onYouTubePlayerReady:', player);
-                (<any>window).player = player;
-                player.addEventListener('onStateChange', (state: number) => {
-                    this.log('onStateChange:', state);
-                    switch (state) {
-                        case YDM.PlayerState.PLAYING:
-                            this.init();
-                            this.randomResume();
-                            break;
-                        case YDM.PlayerState.PAUSED:
-                            this.randomPause();
-                            break;
-                        case YDM.PlayerState.CUED:
-                            this.init();
-                            this.cue();
-                            break;
-                    }
-                });
-                player.addEventListener('onPlaybackRateChange', (rate: number) => {
-                    this.log('onPlaybackRateChange:', rate);
-                });
-                if (typeof oldReady == 'function') {
-                    return oldReady(...args);
-                }
-            };
+            this.hijectYouTubePlayerReady();
         });
     }
 
@@ -52,70 +53,99 @@ export class YDM {
 
     init() {
         this.log('YDM.init');
-        var overlay = document.getElementById('ydm-overlay');
-        var movie_player = document.getElementById('movie_player');
-        if (overlay != null || movie_player == null) return;
-        this.dom['movie_player'] = movie_player;
-        overlay = this.dom['overlay'] = document.createElement('div');
-        overlay.id = 'ydm-overlay';
-        movie_player.appendChild(overlay);
+        this.moviePlayer = document.getElementById('movie_player');
+        if (document.getElementById('ydm-overlay') != null
+            || document.getElementById('movie_player') == null) return;
+        this.moviePlayer.appendChild(this.overlay);
+        let s = new Segment(0, this.moviePlayer.offsetHeight);
+        this.segments[Danmaku.MODE.TOP] = new Segments(s);
+        this.segments[Danmaku.MODE.BOTTOM] = new Segments(s);
+        this.segments[Danmaku.MODE.MARQUEE] = new Segments(s);
     }
 
-    addDanmaku(danmaku: Danmaku) {
-        let e = document.createElement('div');
-        e.classList.add('ydm-danmaku');
-        e.style.top = danmaku.position;
-        var hiddenText = document.createElement('span');
-        hiddenText.classList.add('ydm-danmaku-hidden-text');
-        hiddenText.innerText = danmaku.text;
-        var displayText = document.createElement('span');
-        displayText.classList.add('ydm-danmaku-text');
-        displayText.innerText = danmaku.text;
-        e.appendChild(hiddenText);
-        e.appendChild(displayText);
-        e.addEventListener('animationend', function() {this.remove()}, false);
-        this.dom['overlay'].appendChild(e);
+    addDanmaku(d: Danmaku) {
+        let segments = this.segments[d.mode];
+        let pWidth = this.moviePlayer.offsetWidth;
+        let pHeight = this.moviePlayer.offsetHeight;
+        this.prepare.appendChild(d.e);
+        switch (d.mode) {
+            case Danmaku.MODE.TOP:
+            case Danmaku.MODE.BOTTOM: {
+                d.x = (pWidth+d.width)/2;
+                break;
+            }
+            case Danmaku.MODE.MARQUEE: {
+                d.x = pWidth;
+                break;
+            }
+        }
+        switch (d.mode) {
+            case Danmaku.MODE.TOP:
+            case Danmaku.MODE.MARQUEE: {
+                let s = segments.findFirst(d.height); // bug here
+                segments.subtract(s);
+                d.y = s.end;
+                break;
+            }
+            case Danmaku.MODE.BOTTOM: {
+                let s = segments.findLast(d.height); // bug here
+                segments.subtract(s);
+                d.y = s.start;
+                break;
+            }
+        }
+        this.overlay.appendChild(d.e);
+        this.danmaku[d.mode].push(d);
+    }
+
+    animationFrame(timestamp: number) {
+        if (this.state !== YDM.PlayerState.PLAYING) {
+            this.resumeTimeFrame = null;
+            this.resumeTimeVideo = null
+            return;
+        }
+        if (this.resumeTimeFrame == null)
+            this.resumeTimeFrame = timestamp;
+        if (this.resumeTimeVideo == null)
+            this.resumeTimeVideo = this.currentTime;
+        let time = timestamp - this.resumeTimeFrame + this.resumeTimeVideo;
+        let duration = this.settings['duration'] / this.settings['speed'];
+        let pWidth = this.moviePlayer.offsetWidth;
+        Object.keys(this.danmaku).forEach((mode) => {
+            this.danmaku[mode] = this.danmaku[mode].filter((d) => {
+                let elapsed = time - d.time;
+                if (elapsed >= duration) {
+                    d.e.remove();
+                    return false;
+                }
+                if (mode === Danmaku.MODE.MARQUEE) {
+                    d.x = pWidth - ((pWidth+d.width)*elapsed/duration);
+                    if (d.x + d.width < pWidth)
+                        this.segments[mode].add(d.ySegment);
+                }
+                return true;
+            });
+        });
+        requestAnimationFrame(this.animationFrame.bind(this));
     }
 
     pause() {
-        this.dom['overlay'].classList.add('paused');
+        
     }
 
     resume() {
-        this.dom['overlay'].classList.remove('paused');
+        requestAnimationFrame(this.animationFrame.bind(this));
     }
 
     cue() {
-        let overlay = this.dom['overlay'];
-        while(overlay.firstChild) {
+        let overlay = this.overlay;
+        while(overlay && overlay.firstChild) {
             overlay.removeChild(overlay.firstChild);
         }
     }
 
-    randomInterval: number = null;
-
-    randomInt(min: number, max:number): number {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-
-    randomAddDanmaku() {
-        let danmaku = new Danmaku();
-        danmaku.text = '哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈';
-        danmaku.position = this.randomInt(0,90).toString() + '%';
-        this.addDanmaku(danmaku);
-    }
-
-    randomResume() {
-        if (this.randomInterval != null) return;
-        this.resume();
-        this.randomInterval = setInterval(() => this.randomAddDanmaku(), 1000);
-    }
-
-    randomPause() {
-        if (this.randomInterval == null) return;
-        clearInterval(this.randomInterval);
-        this.pause();
-        this.randomInterval = null;
+    get currentTime(): number {
+        return this.player.getCurrentTime() * 1000;
     }
 
     hijectLoadFunction() {
@@ -130,8 +160,49 @@ export class YDM {
         };
         if ((<any>window).ytplayer.config.args.video_id != null) {
             this.init();
-            this.randomResume();
+            this.resume();
         }
+    }
+
+    hijectYouTubePlayerReady() {
+        let oldReady = (<any>window).onYouTubePlayerReady;
+        (<any>window).onYouTubePlayerReady = (player: any, ...args: any[]) => {
+            this.onYouTubePlayerReady(player);
+            if (typeof oldReady == 'function')
+                return oldReady(player, ...args);
+        };
+    }
+
+    onYouTubePlayerReady(player: any) {
+        this.log('onYouTubePlayerReady:', player);
+        this.player = player;
+        player.addEventListener('onStateChange', (state: number) => {
+            this.log('onStateChange:', state);
+            this.state = state;
+            switch (state) {
+                case YDM.PlayerState.PLAYING:
+                    let d = new Danmaku();
+                    d.text = '哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈';
+                    d.time = this.currentTime;
+                    d.mode = Danmaku.MODE.MARQUEE;
+                    d.size = 25;
+                    d.color = '#FFFFFF';
+                    this.addDanmaku(d);
+                    // this.init();
+                    this.resume();
+                    break;
+                case YDM.PlayerState.PAUSED:
+                    this.pause();
+                    break;
+                case YDM.PlayerState.CUED:
+                    this.init();
+                    this.cue();
+                    break;
+            }
+        });
+        player.addEventListener('onPlaybackRateChange', (rate: number) => {
+            this.log('onPlaybackRateChange:', rate);
+        });
     }
 
 }
