93116f6befbde43cf829d067e85663318d8e2569
Rix <i@rix.li>
2017-02-23 17:39:09 -0500

    Alter content script entry to test out the new YouTubeService class with debug outputs.

--- src/ts/content-script/main.ts
+++ src/ts/content-script/main.ts
@@ -1,6 +1,17 @@
+import { YouTubeService } from './VideoService';
+
 /* If the injection succeeded, this line should print to the console. */
 console.log('__MSG_YDM_welcome_log__');
 
-import { YDM } from './YDM';
+const ydmyt = new YouTubeService();
+(<any>window).ydmyt = ydmyt;
 
-(<any>window).ydm = new YDM();
+ydmyt.onCue.subscribe((video) => console.log('[YDM] Video Cued:', video));
+ydmyt.onPause.subscribe((time) => console.log('[YDM] Video Paused:', time));
+ydmyt.onPlay.subscribe((time) => console.log('[YDM] Video Played:', time));
+ydmyt.onSpeedChange.subscribe((rate) => console.log('[YDM] Video Speed Changed:', rate));
+ydmyt.onScreenDestroy.subscribe(() => console.log('[YDM] Screen Destroyed'));
+ydmyt.onScreenInit.subscribe((screen) => console.log('[YDM] Screen Inited:', screen));
+ydmyt.onScreenResize.subscribe((screen) => console.log('[YDM] Screen Resized:', screen));
+ydmyt.onAdStart.subscribe(() => console.log('[YDM] Ad Started'));
+ydmyt.onAdEnd.subscribe(() => console.log('[YDM] Ad Ended'));
