9424bfeacf942613502847d5b76f8d2caf0286e1
Rix <i@rix.li>
2017-02-23 17:38:18 -0500

    Disable the content script if executing on iframe. Our current method to find the video element is very buggy for general use. Therefore we want to temporarily restrict our target merely on the YouTube site main video page.

--- src/ts/YouDanMu/main.ts
+++ src/ts/YouDanMu/main.ts
@@ -1,5 +1,7 @@
 class App {
     constructor() {
+        // Currently we don't support video in iframe
+        if (this.inIframe()) return;
         window.addEventListener('message', this.messageHandler);
         let req = new XMLHttpRequest();
         req.open('get', chrome.extension.getURL('js/content-script.js'), true);
@@ -7,24 +9,32 @@ class App {
         req.send();
     }
 
-    messageHandler(event : any) : void {
-        if (event.source != window) return;
-        if (event.data.type && (event.data.type == 'FROM_PAGE')) {
-             console.log('[Master]:', event.data);
+    inIframe(): boolean {
+        try {
+            return window.self !== window.top;
+        } catch (e) {
+            return true;
         }
     }
 
-    i18n(str : string) : string {
-        return str.replace(/__MSG_(\w+)__/g, function (match : string, key : string) {
-          return key ? chrome.i18n.getMessage(key) : '';
+    messageHandler(event: any): void {
+        if (event.source != window) return;
+        if (event.data.type && (event.data.type == 'FROM_PAGE')) {
+            console.log('[Master]:', event.data);
+        }
+    }
+
+    i18n(str: string): string {
+        return str.replace(/__MSG_(\w+)__/g, function (match: string, key: string) {
+            return key ? chrome.i18n.getMessage(key) : '';
         });
     }
 
-    inject(func : any) : void {
+    inject(func: any): void {
         let p = document.body || document.head || document.documentElement;
         if (!p) {
-          setTimeout(this.inject.bind(this, func), 0);
-          return;
+            setTimeout(this.inject.bind(this, func), 0);
+            return;
         }
         let script = document.createElement('script');
         script.id = 'ydm-content-script';
