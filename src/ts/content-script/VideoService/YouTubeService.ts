9bfb054dedb8b8d13c1e4330da65287b8ed67c77
Rix <i@rix.li>
2017-02-23 17:35:29 -0500

    Capture and proxy interested YouTube events with RxJS.

--- src/ts/content-script/VideoService/YouTubeService.ts
+++ src/ts/content-script/VideoService/YouTubeService.ts
@@ -1,9 +1,12 @@
 /// <reference path="../../typings/YouTube.d.ts" />
 
 import { Subject } from 'rxjs/Subject';
+import { Observer } from 'rxjs/Observer';
 import { Observable } from 'rxjs/Observable';
 
 import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/operator/sampleTime';
+import 'rxjs/add/operator/multicast';
 
 import { Seconds, Video, Screen, VideoService } from './';
 
@@ -17,11 +20,10 @@ enum State {
     CUED
 }
 
-// TODO:
-//      When video is unplayed, dispatch onScreenDestroy event.
-//      When video is cued, buffering, and immediately after onYouTubePlayerReady,
-//      check for new video, create a new event type onVideo and dispatch it
-//      Capture onScreenResize and onScreenDestroy events
+interface ScreenSize {
+    width: number;
+    height: number;
+}
 
 export class YouTubeService implements VideoService {
     video: Video;
@@ -34,69 +36,138 @@ export class YouTubeService implements VideoService {
     onPause = new Subject<Seconds>();
     onSpeedChange = new Subject<number>();
     onScreenInit = new Subject<Screen>();
-    onScreenResize = new Subject<Screen>();
+    onScreenResize: Observable<Screen>;
     onScreenDestroy = new Subject<void>();
+    onAdStart = new Subject<void>();
+    onAdEnd = new Subject<void>();
 
     private playing = false;
+    private adStarted = false;
     private speedRate = 1;
-    private fullscreen = false;
+    private resizeCaptureInterval: number = null;
+    private _resizeObserver: Subject<Screen>;
 
     constructor() {
+        // Bounces happens when entering fullscreen
+        // Use sampleTime to debound resize events.
+        this.onScreenResize = (<Observable<Screen>>(
+            this._resizeObserver = new Subject<Screen>()
+        ))
+            .sampleTime(1000)
+            .multicast(new Subject<Screen>())
+            .refCount();
         Observable.fromEvent(document, 'DOMContentLoaded')
             .subscribe(() => this.hijectYouTubePlayerReady());
+    }
+
+    cue() {
+        const { player } = this;
+        const videoData = player.getVideoData();
+        if (this.video && this.video.id === videoData.video_id)
+            return;
+        this.video = {
+            id: videoData.video_id,
+            url: player.getVideoUrl(),
+            title: videoData.title,
+            service: 'YouTube',
+            duration: player.getDuration()
+        };
+        this.onCue.next(this.video);
     }
 
     play() {
         if (this.playing) return;
         const { player } = this;
-        const videoData = player.getVideoData();
-        const videoConfig = player.getCurrentVideoConfig();
-        // Check if the screen exists.
-        // If not, create one.
-        if (!this.screen) {
-            const rect = player.getVideoContentRect();
-            this.screen = {
-                width: rect.width,
-                height: rect.height,
-                fullscreen: this.fullscreen
-            };
-            this.onScreenInit.next(this.screen);
-        }
-        // Check if a new video is playing.
-        // If so dispatch the cue event.
-        if (!this.video || videoData.video_id !== this.video.id) {
-            this.video = {
-                id: videoData.video_id,
-                url: player.getVideoUrl(),
-                title: videoData.title,
-                service: 'YouTube',
-                duration: player.getDuration()
-            };
-            this.onCue.next(this.video);
-        }
+        this.screenInit();
+        this.cue();
         this.playing = true;
-        // Verify the video is loaded with config indicating
-        // a main video other on page ads.
-        if (videoConfig.attrs.id === 'movie_player')
-            // Dispatch the play event
-            this.onPlay.next(player.getCurrentTime());
+        this.onPlay.next(player.getCurrentTime());
     }
 
     pause() {
         if (!this.playing) return;
-        // Dispatch the pause event
         this.playing = false;
         this.onPause.next(this.player.getCurrentTime());
-    }
-
-    cue() {
-        
     }
 
     setSpeed(rate: number) {
         if (this.speedRate === rate) return;
         this.speedRate = rate
         this.onSpeedChange.next(rate);
+    }
+
+    screenInit() {
+        if (this.screen) return;
+        const { player } = this;
+        const videoConfig = player.getCurrentVideoConfig();
+        const e = document.getElementById(videoConfig.attrs.id);
+        this.screen = {
+            e: e,
+            width: e.clientWidth,
+            height: e.clientHeight,
+            fullscreen: false
+        };
+        this.startCaptureResize();
+        this.onScreenInit.next(this.screen);
+    }
+
+    screenDestroy() {
+        if (!this.screen) return;
+        this.stopCaptureResize();
+        this.screen = null;
+        this.onScreenDestroy.next();
+    }
+
+    startCaptureResize() {
+        if (this.resizeCaptureInterval != null) return;
+        this.resizeCaptureInterval = setInterval(this.resizeCaptureFn.bind(this), 20);
+    }
+
+    stopCaptureResize() {
+        if (this.resizeCaptureInterval == null) return;
+        clearInterval(this.resizeCaptureInterval);
+        this.resizeCaptureInterval = null;
+    }
+
+    resizeCaptureFn() {
+        if (!this.screen) return;
+        const rect = this.getScreenSize();
+        if (rect.width === this.screen.width &&
+            rect.height === this.screen.height)
+            return;
+        this.screen.width = rect.width;
+        this.screen.height = rect.height;
+        this._resizeObserver.next(this.screen);
+    }
+
+    adStart() {
+        if (this.adStarted) return;
+        this.pause();
+        this.onAdStart.next();
+    }
+
+    adEnd() {
+        if (!this.adStarted) return;
+        this.onAdEnd.next();
+        this.play();
+    }
+
+    unplay() {
+        this.pause();
+        this.screenDestroy();
+    }
+
+    setFullscreen(state: YouTube.FullscreenState) {
+        if (!this.screen) return;
+        if (this.screen.fullscreen == state.fullscreen) return;
+        this.screen.fullscreen = state.fullscreen;
+    }
+
+    getScreenSize(): ScreenSize {
+        return {
+            width: this.screen.e.offsetWidth,
+            height: this.screen.e.offsetHeight
+        };
     }
 
     private onStateChanged(state: State) {
@@ -106,6 +177,11 @@ export class YouTubeService implements VideoService {
                 this.play();
                 break;
             case State.UNSTARTED:
+                this.unplay();
+                break;
+            case State.BUFFERING:
+                this.cue();
+                break;
             case State.CUED:
             case State.PAUSED:
                 this.pause();
@@ -124,13 +200,19 @@ export class YouTubeService implements VideoService {
 
     private onYouTubePlayerReady(player: YouTube.Player) {
         this.player = player;
+        // The injection may happen after the video
+        // starts playing. Here we check the video
+        // state and emmit events immediately
+        this.onStateChanged(player.getPlayerState());
         Observable.fromEvent(player, 'onStateChange')
             .subscribe(this.onStateChanged.bind(this));
         Observable.fromEvent(player, 'onAdStart')
-            .subscribe(this.pause.bind(this));
+            .subscribe(this.adStart.bind(this));
         Observable.fromEvent(player, 'onAdEnd')
-            .subscribe(this.play.bind(this));
+            .subscribe(this.adEnd.bind(this));
         Observable.fromEvent(player, 'onPlaybackRateChange')
             .subscribe(this.setSpeed.bind(this));
+        Observable.fromEvent(player, 'onFullscreenChange')
+            .subscribe(this.setFullscreen.bind(this));
     }
 }
