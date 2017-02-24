acf2d00bbc0ec794345fcf89198c77ec96c13cd1
RixTox <i@rix.li>
2017-02-24 17:28:01 -0500

    Due to semantic use of "content-script" specified by the Google Chrome Extension documentation, we decided to swap the naming of the YouDanMu.js and content-script.js. To prevent confusion, the new "content-script.js" should run in an isolated environment, without direct access to the page's `window` object. While the new "YouDanMu.js" should be injected onto the page in a <script> tag, and thus has direct access to page's `window` object, but lose the access of `chrome.extension` APIs.

--- src/ts/YouDanMu/main.ts
+++ src/ts/YouDanMu/main.ts
@@ -1,47 +1,13 @@
-class App {
-    constructor() {
-        // Currently we don't support video in iframe
-        if (this.inIframe()) return;
-        window.addEventListener('message', this.messageHandler);
-        let req = new XMLHttpRequest();
-        req.open('get', chrome.extension.getURL('js/content-script.js'), true);
-        req.onload = () => this.inject(req.responseText);
-        req.send();
-    }
+import { Logger } from './util';
+import { YouTubeService } from './VideoService';
 
-    inIframe(): boolean {
-        try {
-            return window.self !== window.top;
-        } catch (e) {
-            return true;
-        }
-    }
+// Enable development level logging
+Logger.debugLevel = 3;
 
-    messageHandler(event: any): void {
-        if (event.source != window) return;
-        if (event.data.type && (event.data.type == 'FROM_PAGE')) {
-            console.log('[Master]:', event.data);
-        }
-    }
+const console = new Logger('Main');
 
-    i18n(str: string): string {
-        return str.replace(/__MSG_(\w+)__/g, function (match: string, key: string) {
-            return key ? chrome.i18n.getMessage(key) : '';
-        });
-    }
+/* If the injection succeeded, this line should print to the console. */
+console.log(0, '__MSG_YDM_welcome_log__');
 
-    inject(func: any): void {
-        let p = document.body || document.head || document.documentElement;
-        if (!p) {
-            setTimeout(this.inject.bind(this, func), 0);
-            return;
-        }
-        let script = document.createElement('script');
-        script.id = 'ydm-content-script';
-        script.setAttribute('type', 'text/javascript');
-        script.appendChild(document.createTextNode(this.i18n('(function(){' + func.toString() + '})();')));
-        p.appendChild(script);
-    }
-}
-
-new App();
+const ydmyt = new YouTubeService();
+(<any>window).ydmyt = ydmyt;
